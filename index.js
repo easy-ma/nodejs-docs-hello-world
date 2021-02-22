5a11b35edfa32631f75d63bf79500699858ab33c
Babidiii <bilal.guillaumie@etu.uca.fr>
2021-02-23 00:54:14 +0100

    feat: added stage

--- index.js
+++ index.js
@@ -2,9 +2,7 @@ const http = require("http");
 
 const server = http.createServer((request, response) => {
   response.writeHead(200, { "Content-Type": "text/plain" });
-  response.end(
-    "Hello World, Ceci est notre projet Azure: Guillaumie Bilal & Nonclercq Basile !"
-  );
+  response.end("Hello Stage, Ceci est notre projet Azure Version STAGING");
 });
 
 const port = process.env.PORT || 1337;
