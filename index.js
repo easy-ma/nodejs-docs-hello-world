1b1e8e70ad292b5ed1b819336d5b8935ccae23af
Babidiii <bilal.guillaumie@etu.uca.fr>
2021-02-23 00:48:15 +0100

    update result

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
-const http = require('http');
+const http = require("http");
 
 const server = http.createServer((request, response) => {
-    response.writeHead(200, {"Content-Type": "text/plain"});
-    response.end("Hello World!");
+  response.writeHead(200, { "Content-Type": "text/plain" });
+  response.end(
+    "Hello World, Ceci est notre projet Azure: Guillaumie Bilal & Nonclercq Basile !"
+  );
 });
 
 const port = process.env.PORT || 1337;
