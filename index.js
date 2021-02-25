3344739aea8b141bb3417758ea9719f8214f8741
Babidiii <bilal.guillaumie@etu.uca.fr>
2021-02-25 15:18:14 +0100

    feat: vuln added

--- index.js
+++ index.js
@@ -1,10 +1,17 @@
 const http = require("http");
+const url = require("url");
 
 const server = http.createServer((request, response) => {
-  response.writeHead(200, { "Content-Type": "text/plain" });
-  response.end(
-    "Hello World, Ceci est notre projet Azure: Guillaumie Bilal & Nonclercq Basile !"
-  );
+  const queryData = url.parse(request.url, true).query;
+  response.writeHead(200, { "Content-Type": "text/html" });
+
+  if (queryData.name) {
+    // user told us their name in the GET request, ex: http://host:8000/?name=Tom
+
+    response.end("Hello " + queryData.name);
+  } else {
+    response.end("Hello World call url/?name='yourname'\n");
+  }
 });
 
 const port = process.env.PORT || 1337;
